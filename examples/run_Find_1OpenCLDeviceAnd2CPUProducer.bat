db7d67bd94553a0e79cc5c9ae5e27a24d0243f88
Bernard Ladenthin <bernard.ladenthin@gmail.com>
2022-12-19 17:49:47 +0100

    Increase version to 1.1.0-SNAPSHOT. Improve and arrange argLine.

--- examples/run_Find_1OpenCLDeviceAnd2CPUProducer.bat
+++ examples/run_Find_1OpenCLDeviceAnd2CPUProducer.bat
@@ -1,9 +1,16 @@
 rem start /low java ^
 java ^
+--illegal-access=permit ^
+--add-opens java.base/java.lang=ALL-UNNAMED ^
+--add-opens java.base/java.io=ALL-UNNAMED ^
+--add-opens java.base/java.nio=ALL-UNNAMED ^
+--add-opens java.base/jdk.internal.ref=ALL-UNNAMED ^
+--add-opens java.base/sun.nio.ch=ALL-UNNAMED ^
+--add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED ^
 -server ^
 -Xms512M ^
 -Xmx16G ^
 -Dlogback.configurationFile=logbackConfiguration.xml ^
 -jar ^
-bitcoinaddressfinder-1.0.0-SNAPSHOT-jar-with-dependencies.jar ^
+bitcoinaddressfinder-1.1.0-SNAPSHOT-jar-with-dependencies.jar ^
 config_Find_1OpenCLDeviceAnd2CPUProducer.js >> log_Find_1OpenCLDeviceAnd2CPUProducer.txt 2>&1
