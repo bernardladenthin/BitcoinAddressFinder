7136b3544a3cc22f6901126e1f061cb292c4ff57
cjherm <akbigchris@gmail.com>
2023-05-19 23:35:50 +0200

    Light refactoring of "OpenCLGridResult"

--- src/main/java/net/ladenthin/bitcoinaddressfinder/OpenCLGridResult.java
+++ src/main/java/net/ladenthin/bitcoinaddressfinder/OpenCLGridResult.java
@@ -1,19 +1,18 @@
 // @formatter:off
-/**
+/*
  * Copyright 2020 Bernard Ladenthin bernard.ladenthin@gmail.com
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 // @formatter:on
 package net.ladenthin.bitcoinaddressfinder;
@@ -24,6 +23,7 @@ import java.nio.ByteBuffer;
 public class OpenCLGridResult {
 
     private final ByteBufferUtility byteBufferUtility = new ByteBufferUtility(true);
+
     private final BigInteger[] secretKeys;
     private final int workSize;
     private final boolean chunkMode;
@@ -49,22 +49,20 @@ public class OpenCLGridResult {
         }
     }
 
-    public int getWorkSize() {
-        return workSize;
-    }
-
     public ByteBuffer getResult() {
         return result;
     }
-    
+
+    /**
+     * Frees byte buffer and sets result to NULL
+     */
     public void freeResult() {
-        // free and do not use anymore
         byteBufferUtility.freeByteBuffer(result);
         result = null;
     }
-    
+
     /**
-     * Time consuming.
+     * @return the calculated public keys
      */
     public PublicKeyBytes[] getPublicKeyBytes() {
         PublicKeyBytes[] publicKeys = new PublicKeyBytes[workSize];
@@ -81,50 +79,40 @@ public class OpenCLGridResult {
         }
         return publicKeys;
     }
-    
+
     /**
      * Read the inner bytes in reverse order.
      */
-    private static final PublicKeyBytes getPublicKeyFromByteBufferXY(ByteBuffer b, int keyNumber, BigInteger secretKeyBase) {
+    private static PublicKeyBytes getPublicKeyFromByteBufferXY(ByteBuffer b, int keyNumber,
+                                                               BigInteger secretKeyBase) {
+
         BigInteger secret = AbstractProducer.calculateSecretKey(secretKeyBase, keyNumber);
-        if(BigInteger.ZERO.equals(secret)) {
-            // the calculated key is invalid, return a fallback
+
+        if (BigInteger.ZERO.equals(secret)) {
             return PublicKeyBytes.INVALID_KEY_ONE;
         }
+
         byte[] uncompressed = new byte[PublicKeyBytes.PUBLIC_KEY_UNCOMPRESSED_BYTES];
         uncompressed[0] = PublicKeyBytes.PARITY_UNCOMPRESSED;
-        
-        int keyOffsetInByteBuffer = PublicKeyBytes.TWO_COORDINATES_NUM_BYTES*keyNumber;
-        
+
+        // Same way as in OpenCL kernel:
+        // int r_offset = PUBLIC_KEY_LENGTH_X_Y_WITHOUT_PARITY * global_id;
+        int keyOffsetInByteBuffer = PublicKeyBytes.TWO_COORDINATES_NUM_BYTES * keyNumber;
+
         // read ByteBuffer
         byte[] yx = new byte[PublicKeyBytes.TWO_COORDINATES_NUM_BYTES];
         for (int i = 0; i < PublicKeyBytes.TWO_COORDINATES_NUM_BYTES; i++) {
-            yx[yx.length-1-i] = b.get(keyOffsetInByteBuffer+i);
+            yx[yx.length - 1 - i] = b.get(keyOffsetInByteBuffer + i);
         }
-        
+
         // copy x
-        System.arraycopy(yx, PublicKeyBytes.ONE_COORDINATE_NUM_BYTES, uncompressed, PublicKeyBytes.PARITY_BYTES_LENGTH, PublicKeyBytes.ONE_COORDINATE_NUM_BYTES);
+        System.arraycopy(yx, PublicKeyBytes.ONE_COORDINATE_NUM_BYTES, uncompressed, PublicKeyBytes.PARITY_BYTES_LENGTH,
+                PublicKeyBytes.ONE_COORDINATE_NUM_BYTES);
         // copy y
-        System.arraycopy(yx, 0, uncompressed, PublicKeyBytes.PARITY_BYTES_LENGTH+PublicKeyBytes.ONE_COORDINATE_NUM_BYTES, PublicKeyBytes.ONE_COORDINATE_NUM_BYTES);
-        
-        if (false) {
-            assertValidResult(uncompressed);
-        }
-        PublicKeyBytes publicKeyBytes = new PublicKeyBytes(secret, uncompressed);
-        return publicKeyBytes;
+        System.arraycopy(yx, 0, uncompressed,
+                PublicKeyBytes.PARITY_BYTES_LENGTH + PublicKeyBytes.ONE_COORDINATE_NUM_BYTES,
+                PublicKeyBytes.ONE_COORDINATE_NUM_BYTES);
+
+        return new PublicKeyBytes(secret, uncompressed);
     }
-    
-    private static void assertValidResult(byte[] uncompressed) {
-        boolean invalid = true;
-        for (int i = 1; i < uncompressed.length; i++) {
-            if (uncompressed[i] != 0) {
-                invalid = false;
-                break;
-            }
-        }
-        if (invalid) {
-            throw new RuntimeException("Invalid result from GPU, all uncompressed key bytes are 0.");
-        }
-    }
-    
 }
